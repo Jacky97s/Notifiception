18b0bf879facc02f50689a28beea17bbd7806f98
Jacky97s <d0526378@mail.fcu.edu.tw>
2018-05-10 19:07:46 +0800

    nav item link to fragment

--- app/src/main/java/app/fcu/notifiception/MainActivity.java
+++ app/src/main/java/app/fcu/notifiception/MainActivity.java
@@ -1,36 +1,115 @@
 package app.fcu.notifiception;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    FragmentTransaction fragmentTransaction;
+    NavigationView navigationView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        //把 toolbar 的 layout 固定
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
+        //叫出 Navigation，抽屜的概念
+        drawerLayout = findViewById(R.id.drawer_Layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //預設開啟 Notifiception fragment
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, new Notifiception());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle("Notifiception");
+
+        //當 Navigation 的 item 被按下，開啟該 item fragment
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.pg_noti:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new Notifiception());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Notifiception");
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.pg_br:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new BatteryReminder());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Battery Reminder");
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.pg_tos:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new TurnOffSilent());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Turn Off Silent");
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.pg_police:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new PoliceData());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Emergency Contact");
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.pg_donate:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new Donate());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Donation");
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.pg_mail:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new Mail());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Feedback");
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.pg_about:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new About());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("About Us");
+                        drawerLayout.closeDrawers();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
     }
 }
---
+++ app/src/main/java/app/fcu/notifiception/Notifiception.java
@@ -0,0 +1,29 @@
+package app.fcu.notifiception;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Notifiception extends Fragment {
+
+
+    public Notifiception() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notifiception, container, false);
+    }
+
+}
