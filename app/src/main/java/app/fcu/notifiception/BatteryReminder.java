de095bdca0dd1512ae18cc3f098715a41066a53f
Jacky97s <d0526378@mail.fcu.edu.tw>
2018-05-22 20:17:49 +0800

    battery_reminder and color

--- app/src/main/java/app/fcu/notifiception/BatteryReminder.java
+++ app/src/main/java/app/fcu/notifiception/BatteryReminder.java
@@ -2,27 +2,47 @@ package app.fcu.notifiception;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BatteryReminder extends Fragment {
-
+public class BatteryReminder extends Fragment{
 
     public BatteryReminder() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        SeekBar seekBar = container.findViewById(R.id.br_seekBar);
+        final TextView seekValue = container.findViewById(R.id.seekValue);
+
+//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//        });
+
         return inflater.inflate(R.layout.fragment_battery_reminder, container, false);
     }
 
