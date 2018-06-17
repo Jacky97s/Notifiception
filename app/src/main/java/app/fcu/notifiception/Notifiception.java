b60f8829b0a7dd67af4a00721c202e5d77462b6d
D0426496 <paultang630@gmail.com>
2018-06-18 01:40:38 +0800

    save notifiception's user setting

--- app/src/main/java/app/fcu/notifiception/Notifiception.java
+++ app/src/main/java/app/fcu/notifiception/Notifiception.java
@@ -1,38 +1,122 @@
 package app.fcu.notifiception;
 
 
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Notifiception extends Fragment {
-
+    public static final String TAG = "Service";
 
     public Notifiception() {
         // Required empty public constructor
     }
 
+    private Switch startService;
+
+    private Switch stopService;
+
+    int counter;
+    int num;
+
+    Spinner spinner;
+    Switch msgSwitch;
+
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "onReceiver() executed");
+            num = Integer.valueOf(intent.getStringExtra("Counter"));
+            if (num < counter) {
+                Toast toast = Toast.makeText(getActivity(), "Time to check your new msg", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         View v = inflater.inflate(R.layout.fragment_notifiception, container, false);
-        Spinner spinner = v.findViewById(R.id.timeSpinner);
+        spinner = v.findViewById(R.id.timeSpinner);
+        msgSwitch = v.findViewById(R.id.msgSwitch);
+
+        //load xml
+        SharedPreferences pref = getActivity().getSharedPreferences("Notifiception", Context.MODE_PRIVATE);
+        int spinnerPosition = pref.getInt("Counter", 5);
+        boolean switchState = pref.getBoolean("OnOff", true);
+        Log.d(TAG, "spinnerPos: " + spinnerPosition);
+
+        msgSwitch.setOnCheckedChangeListener(state);
+        msgSwitch.setChecked(switchState);
+
         ArrayAdapter<CharSequence> nAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.hourForSpinner,
                 R.layout.spinner_text);
         nAdapter.setDropDownViewResource(R.layout.spinner_style);
         spinner.setAdapter(nAdapter);
+        spinner.setSelection(spinnerPosition, true);
+        counter = spinner.getSelectedItemPosition();
         return v;
     }
 
+    CompoundButton.OnCheckedChangeListener state = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean check) {
+            Log.d(TAG, "onCheckedChanged() executed");
+            if (check) {
+
+                //save
+                SharedPreferences pref = getActivity().getSharedPreferences("Notifiception", Context.MODE_PRIVATE);
+                SharedPreferences.Editor prefEdit = pref.edit();
+                prefEdit.putInt("Counter", counter);
+                prefEdit.putBoolean("OnOff", true);
+                prefEdit.commit();
+
+                //start timer
+//                getActivity().registerReceiver(receiver, new IntentFilter("Action"));
+                Intent startIntent = new Intent(getActivity(), NotificeptionService.class);
+                startIntent.putExtra("Counter", String.valueOf(counter));
+                getActivity().startService(startIntent);
+
+
+            } else {
+                //save
+                SharedPreferences pref = getActivity().getSharedPreferences("Notifiception", Context.MODE_PRIVATE);
+                SharedPreferences.Editor prefEdit = pref.edit();
+                prefEdit.putInt("Counter", counter);
+                prefEdit.putBoolean("OnOff", false);
+                prefEdit.commit();
+
+                //stop timer
+                Intent stopIntent = new Intent(getActivity(), NotificeptionService.class);
+                getActivity().stopService(stopIntent);
+            }
+
+        }
+    };
 }
---
+++ app/src/main/java/app/fcu/notifiception/NotificeptionService.java
@@ -0,0 +1,68 @@
+package app.fcu.notifiception;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class NotificeptionService extends Service {
+    public static final String TAG = "Service";
+
+    private Timer timer;
+    private TimerTask task;
+    private int counter;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate() executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        counter = Integer.valueOf(intent.getStringExtra("Counter"))+5;
+        Log.d(TAG, "onStartCommand() executed");
+        startTimer();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        stopTimer();
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() executed");
+    }
+
+    public void startTimer() {
+        timer = new Timer();
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                counter--;
+                Log.d(TAG, "startTimer() executed");
+                Intent i1 = new Intent();
+                i1.setAction("action");
+                i1.putExtra("Counter", String.valueOf(counter));
+                sendBroadcast(i1);
+            }
+        };
+        timer.schedule(task, 0, 1000);
+    }
+
+    public void stopTimer() {
+        Log.d(TAG, "stopTimer() executed");
+        timer.cancel();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+}
